cd39d2b7e341098fc030b6b496219047bdb4a404
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-05-16 09:36:29 +0700

    optimize listing contact

--- app/src/main/java/com/example/dminh/contactusingfragment/repository/ReadContactRepo.kt
+++ app/src/main/java/com/example/dminh/contactusingfragment/repository/ReadContactRepo.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.content.Context
 import android.provider.ContactsContract
 import android.util.Log
+import android.widget.Toast
 import com.example.dminh.contactusingfragment.models.MyContact
 import java.util.ArrayList
 
@@ -12,38 +13,21 @@ class ReadContactRepo(private val context: Context?){
     fun readContact() : MutableLiveData<List<MyContact>>{
         val contacts = ArrayList<MyContact>()
         val resolver = context?.contentResolver
-        val cursor = resolver?.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null)!!
+        val cursor = resolver?.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null)!!
 
         if (cursor.count > 0) {
             while (cursor.moveToNext()) {
-                val contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
                 val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
 
-                var phoneNumber: String? = null
+                val phoneNumber: String? = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
 
-                if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-                    val pCur = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", arrayOf<String>(contactId), null)!!
-
-                    while (pCur.moveToNext()) {
-                        val phoneType = pCur.getInt(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.TYPE))
-                        phoneNumber = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                        when (phoneType) {
-                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE -> Log.e("$name(mobile number)", phoneNumber)
-                            ContactsContract.CommonDataKinds.Phone.TYPE_HOME -> Log.e("$name(home number)", phoneNumber)
-                            ContactsContract.CommonDataKinds.Phone.TYPE_WORK -> Log.e("$name(work number)", phoneNumber)
-                            ContactsContract.CommonDataKinds.Phone.TYPE_OTHER -> Log.e("$name(other number)", phoneNumber)
-                            else -> {
-                            }
-                        }
-                    }
-                    pCur.close()
-                }
                 val contact = MyContact(name, phoneNumber)
                 contacts.add(contact)
             }
-        } else
+        } else{
             Log.d("contact", "data not found")
+            Toast.makeText(context, "data not found", Toast.LENGTH_SHORT).show()
+        }
         cursor.close()
         val results = MutableLiveData<List<MyContact>>()
         results.value = contacts
